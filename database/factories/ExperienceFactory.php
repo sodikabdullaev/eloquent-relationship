937882b2879c1ec7de3e53182045262fab55fd87
sodik abdullaev <sodiqabdullaev@mail.ru>
2024-04-16 22:00:39 +0100

    database migrations and seeding

---
+++ database/factories/ExperienceFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Experience>
+ */
+class ExperienceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'points' => fake()->numberBetween(0, 100),
+        ];
+    }
+}
